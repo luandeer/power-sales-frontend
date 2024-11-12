faea3c2eb76584c9f9ea819286b6306ea9b2abdf
alexander <alexander_pere@hotmail.com>
2024-11-12 16:43:16 -0500

    .

--- src/components/personalized/Form.tsx
+++ src/components/personalized/Form.tsx
@@ -157,9 +157,6 @@ export default function UserForm({
 														field.value ? new Date(field.value) : undefined
 													}
 													onSelect={field.onChange}
-													disabled={(date) =>
-														date > new Date() || date < new Date('1900-01-01')
-													}
 													initialFocus
 												/>
 											</PopoverContent>
